c8511d959c62bd971a43ef3389f7984598399b02
Nenad199822 <neso.zabjelo@gmail.com>
2023-09-14 21:59:02 +0200

    Make operate function and change a little style on css for buttons and add just some text to screen to see is it work

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ function divide(a, b) {
 let firstNumber = "";
 let secondNumber = "";
 let operator = "";
+let numbers = document.querySelectorAll('.number')
+function operate(firstNumber, operator, secondNumber) {
+    if (operator == "+") {
+        add(firstNumber.parseInt(), secondNumber.parseInt());
+    } else if (operator == "-") {
+        subtract(firstNumber.parseInt(), secondNumber.parseInt());
+    } else if (operator == "*") {
+        multiply(firstNumber.parseInt(), secondNumber.parseInt());
+    } else if (operator == "/") {
+        divide(firstNumber.parseInt(), secondNumber.parseInt());
+    }
+}
