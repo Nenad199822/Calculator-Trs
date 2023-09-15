9fd83ff944963398bbfb62423c53a51b54b7e195
Nenad199822 <neso.zabjelo@gmail.com>
2023-09-15 15:13:02 +0200

    90% of js is removed i need to change everything because code dont work

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    displayValue();
-    selectedOperator();
-    operate(firstNumber, currentOperator, secondNumber)
+
 })
 function add(a, b) {
     return a + b;
@@ -15,55 +13,4 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
-let screenText = document.querySelector('.screen-text')
-let firstNumber = "";
-let secondNumber = "";
-let currentOperator = "";
-let enteringFirstNumber = true;
-let numbers = document.querySelectorAll('.number')
-function operate(firstNumber, currentOperator, secondNumber) {
-    let result;
-    if (currentOperator == "+") {
-        result = add(firstNumber.parseInt(), secondNumber.parseInt());
-    } else if (currentOperator == "-") {
-        result = subtract(firstNumber.parseInt(), secondNumber.parseInt());
-    } else if (currentOperator == "*") {
-        result = multiply(firstNumber.parseInt(), secondNumber.parseInt());
-    } else if (currentOperator == "/") {
-        if (firstNumber === 0 || secondNumber === 0) {
-            result = 0;
-        } else {
-            result = divide(firstNumber.parseInt(), secondNumber.parseInt());
-        }
-    }
-    return result;
-}
-function displayValue() {
-    numbers.forEach(number => {
-        number.addEventListener('click', function () {
-            const clickedNumber = number.innerText;
-            if (enteringFirstNumber) {
-                firstNumber += clickedNumber;
-                screenText.innerText = firstNumber;
-            } else {
-                secondNumber += clickedNumber;
-                screenText.innerText = secondNumber
-            }
-        });
 
-    });
-}
-let operators = document.querySelectorAll('.operator');
-function selectedOperator(currentOperator) {
-    operators.forEach(operator => {
-        operator.addEventListener('click', function () {
-            currentOperator = operator.innerText
-            screenText.innerText = currentOperator;
-        })
-    })
-}
-let equal = document.getElementById('operator-equal')
-equal.addEventListener('click', function () {
-    result = operate(parseFloat(firstNumber), currentOperator, parseFloat(secondNumber));
-    screenText.innerText = result;
-})
