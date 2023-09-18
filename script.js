c4a56edee41109b07d4b8a5dbf7a6060d86c01b7
Nenad199822 <neso.zabjelo@gmail.com>
2023-09-18 19:28:20 +0200

    Make numbers to handle on click and add values and op variables

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
+let operator = " ";
+let previousValue = "";
+let currentValue = ""
+
+
 document.addEventListener('DOMContentLoaded', function () {
     let clear = document.querySelector('.clear')
     let equal = document.querySelector('.operator-equal')
-    let numbers = document.querySelectorAll('.numbers')
+    let numbers = document.querySelectorAll('.number')
     let operators = document.querySelectorAll('.operator')
+    let previousScreen = document.querySelector('.previous')
+    let currentScreen = document.querySelector('.current')
+    numbers.forEach((number) => number.addEventListener('click', function (e) {
+        handleNumber(e.target.textContent)
+        currentScreen.textContent = currentValue
+    }))
 
 })
 function add(a, b) {
@@ -17,4 +28,9 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+function handleNumber(num) {
+    if (currentValue.length < 5) {
+        currentValue += num
+    }
 
+}
